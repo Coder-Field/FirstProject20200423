b863096e4fa30fee60faa79d81568e4040bbe410
bxc <634027692@qq.com>
2020-04-23 20:13:08 +0800

    test sucess

--- 0423.c
+++ 0423.c
@@ -1,12 +1,11 @@
 1 this is my branch master for test git
 
-
-
 2 this code make it on the ubuntu1404 systerm on 20200423
-
   
 3 this code make it on github , using get pull to downlond this code.....
 
 4 test git push on ubuntu1404
 
 5 test git pull on GitHub on the web
+
+666 test sucess
